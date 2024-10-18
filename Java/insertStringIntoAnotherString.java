f3e02698513985f1575c7b9dc319364e243d06ec
RutujaWayase <rutujawayase1234@gmail.com>
2024-10-19 00:09:34 +0530

    adding insert

--- Java/insertStringIntoAnotherString.java
+++ Java/insertStringIntoAnotherString.java
@@ -3,11 +3,26 @@ public class insertStringIntoAnotherString {
         String originaString, 
         String stringToBeInserted,
         int index){
+            //Create a new String
+            String newString = new String();
+            for(int i=0; i<originaString.length(); i++){
+                //Insert the original string character
+                //into the new string
+                newString += originaString.charAt(i);
+                if(i == index){
+                    //Insert the string to be inserted
+                    //into the new string
+                    newString += stringToBeInserted;
+                }
+            }
+            //return the modified string
+            return newString;
 
     }
     public static void main(String[] args) {
         //Get the strings
-        String originalString = "GreeksGreeks";
+        //String originalString = "GreeksGreeks";
+        String originalString = "Good Fortune";
         String stringToBeInserted = "For";
         int index = 4;
 
@@ -16,7 +31,7 @@ public class insertStringIntoAnotherString {
         System.out.println("String to be inserted at index: "+ index);
 
         //Insert the string
-        System.out.println("Modified string: "+);
+        System.out.println("Modified string: "+insertString(originalString, stringToBeInserted, index));
     }
     
 }
